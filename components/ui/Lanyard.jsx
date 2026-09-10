0009f246f8b039cf64999fe9e27e08fc254c7286
NAVEEN060241 <naveen06akn@gmail.com>
2026-09-10 17:28:34 +0530

    card adjustment

--- components/ui/Lanyard.jsx
+++ components/ui/Lanyard.jsx
@@ -169,7 +169,7 @@ function Band({ isMobile, frontImage, backImage, imageFit, lanyardImage, lanyard
 
   return (
     <>
-      <group position={[0, 5.5, 0]}>
+      <group position={[0, 3.15, 0]}>
         <RigidBody ref={fixed} {...segmentProps} type="fixed" />
         <RigidBody position={[0.5, 0, 0]} ref={j1} {...segmentProps}><BallCollider args={[0.1]} /></RigidBody>
         <RigidBody position={[1, 0, 0]} ref={j2} {...segmentProps}><BallCollider args={[0.1]} /></RigidBody>
